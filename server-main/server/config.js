5b0d16d090df00dd6424dbdbe0876da46dc30876
gfed53 <gfederico7153@gmail.com>
2017-12-16 13:14:39 -0500

    Isolated front and backend - backend is now under a different project directory for now.

---
+++ server-main/server/config.js
@@ -0,0 +1,27 @@
+exports.PORT = process.env.PORT || 3000;
+
+exports.KEYS = {
+	igdbKey: 'XXX'
+};
+
+exports.DATABASE_URL = process.env.DATABASE_URL ||
+global.DATABASE_URL ||
+(process.env.NODE_ENV === 'production' ?
+	'x' :
+	'mongodb://localhost/igdb');
+
+
+// mLab (production):
+
+// mongodb://mLab_USER_NAME:PASSWORD@ds#.mlab.com:#/DB-NAME
+
+// eg. 
+// mongodb://gfed123:notmypassword@ds155841.mlab.com:55841/heavenly-whirlwind
+
+
+// Local:
+
+// mongodb://localhost/COLLECTION_NAME
+
+// eg. 
+// mongodb://localhost/todos
