faecc3592b76bdeb6b650a40b06a675a6dec7477
gfed53 <gfederico7153@gmail.com>
2018-02-10 12:08:55 -0500

    Single Game: added autoscroll after searching single game - mainly for smaller viewports.

--- client-main/src/app/single-game/single-game.component.ts
+++ client-main/src/app/single-game/single-game.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { PageScrollConfig, PageScrollService, PageScrollInstance } from 'ng2-page-scroll';
 
 import { SingleSearchService } from '../services/single-search.service';
 import { SingleGameService } from '../services/single-game.service';
@@ -12,7 +14,21 @@ import { SingleGameService } from '../services/single-game.service';
 export class SingleGameComponent implements OnInit {
   
 
-  constructor(private singleGameService: SingleGameService) { 
+  constructor(
+    private singleGameService: SingleGameService,
+    private pageScrollService: PageScrollService,
+    @Inject(DOCUMENT) private document: any
+  ) { 
+    PageScrollConfig.defaultDuration = 750;
+    PageScrollConfig.defaultEasingLogic = {
+      ease: (t: number, b: number, c: number, d: number): number => {
+        // quadratic ease in/out
+        t /= d/2;
+        if (t < 1) return c/2*t*t + b;
+        t--;
+        return -c/2 * (t*(t-2) - 1) + b;
+      }
+    };
   }
 
   singleSearchResults: any[];
@@ -23,12 +39,19 @@ export class SingleGameComponent implements OnInit {
     this.singleGameService.singleSearchResults$
     .subscribe(list => {
       this.singleSearchResults = list;
+      // Wait a tick
+      setTimeout(this.scrollDown.bind(this), 0);
     });
 
     this.singleGameService.currentGame$
     .subscribe(game => {
       this.selectedResult = game;
     });
+  }
+
+  public scrollDown(): void {
+    let pageScrollInstance: PageScrollInstance = PageScrollInstance.simpleInstance(this.document, '#single-result');
+    this.pageScrollService.start(pageScrollInstance);
   }
 
 }
