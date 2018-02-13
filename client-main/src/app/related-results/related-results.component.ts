b4d4812aca63abca67465ebfe41f41210e315411
gfed53 <gfederico7153@gmail.com>
2018-02-13 12:52:20 -0500

    Related Results: user can click button to set selected game as basis of next search. Added some styles to button.

--- client-main/src/app/related-results/related-results.component.ts
+++ client-main/src/app/related-results/related-results.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, OnChanges, Input } from '@angular/core';
 
+import { SingleGameService } from '../services/single-game.service';
+
+
 @Component({
   selector: 'app-related-results',
   templateUrl: './related-results.component.html',
@@ -9,7 +12,9 @@ export class RelatedResultsComponent implements OnInit {
 
   @Input() relatedResults: any;
 
-  constructor() { }
+  constructor(
+    private singleGameService: SingleGameService
+  ) { }
 
   ngOnInit() {
   }
@@ -20,6 +25,12 @@ export class RelatedResultsComponent implements OnInit {
   // For game item, sets value on item object indicating that image has been loaded
   onImageLoad(item) {
     item.imageLoaded = true;
+  }
+
+  // Use a related search result as the basis 
+  updateSingleResult(item){
+    let a = [item];
+    this.singleGameService.updateResults(a);
   }
 
 }
