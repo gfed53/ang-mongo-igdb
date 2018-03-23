d0b57118c79f3a001f1c86ec4ed5df337e6282e5
gfed53 <gfederico7153@gmail.com>
2018-03-23 14:27:30 -0400

    Single Search: added fade/slidein animation for single result. Some code cleanup.

--- src/app/single-game/single-game.component.ts
+++ src/app/single-game/single-game.component.ts
@@ -2,6 +2,15 @@ import { Component, Inject, OnInit } from '@angular/core';
 import { DOCUMENT } from '@angular/common';
 import { PageScrollConfig } from 'ng2-page-scroll';
 
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition
+} from '@angular/animations';
+
+
 import { SingleSearchService } from '../services/single-search.service';
 import { SingleGameService } from '../services/single-game.service';
 import { SmoothScrollService } from '../services/smooth-scroll.service';
@@ -10,7 +19,19 @@ import { SmoothScrollService } from '../services/smooth-scroll.service';
 @Component({
   selector: 'app-single-game',
   templateUrl: './single-game.component.html',
-  styleUrls: ['./single-game.component.scss']
+  styleUrls: ['./single-game.component.scss'],
+  // TODO: with all animations, store animations array in service and reference ones we need so we don't repeat so much? Maybe call it animationsCollection in service.
+  animations: [
+    trigger('fadeSlideInOut', [
+      transition('void => *', [
+        style({transform: 'translateX(-2em)', opacity: 0}),
+        animate(200, style({transform: 'translateX(0)', opacity: 1}))
+      ]),
+      transition('* => void', [
+        animate(200, style({transform: 'translateX(-2em)', opacity: 0}))
+      ])
+    ])
+  ],
 })
 export class SingleGameComponent implements OnInit {
   
